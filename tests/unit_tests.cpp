2ff02afbaf2b1eb8f66e1d7e88ed0098b173d8f2
Bogdan Cristea <cristeab@gmail.com>
2013-08-05 14:06:23 +0200

    add biquad filter test

--- tests/unit_tests.cpp
+++ tests/unit_tests.cpp
@@ -8,6 +8,7 @@ class TestGestureAlgos: public QObject
     Q_OBJECT
 private slots:
     void imageToScreenFilterKalman();
+	void filterBiquad();
 };
 
 void TestGestureAlgos::imageToScreenFilterKalman()
@@ -31,6 +32,35 @@ void TestGestureAlgos::imageToScreenFilterKalman()
 	QVERIFY(EXIT_SUCCESS == algos->imageToScreen(x, y));
 	QVERIFY(1024.0 == x);
 	QVERIFY(0.0 == y);
+}
+
+void TestGestureAlgos::filterBiquad()
+{
+	GestureAlgos *algos = GestureAlgos::instance();
+	QVERIFY(NULL != algos);
+
+	float fn = static_cast<float>(0.1);
+	const static float PI = static_cast<float>(3.1415926535);
+	const static float TOL = static_cast<float>(1e-3);
+	int nbSamp = static_cast<int>(10/fn);
+	float val = 0;
+	float sum = 0;
+	for (int n = 0; n < nbSamp; ++n) {
+		val = sin(2*PI*fn*n);
+		algos->filterBiquad(val);
+		sum += abs(val*val);
+	}
+	QVERIFY(fabs(48.5314 - sum) < TOL);
+
+	fn = static_cast<float>(0.4);
+	nbSamp = static_cast<int>(10/fn);
+	sum = 0;
+	for (int n = 0; n < nbSamp; ++n) {
+		val = sin(2*PI*fn*n);
+		algos->filterBiquad(val);
+		sum += abs(val*val);
+	}
+	QVERIFY(fabs(1.58709 - sum) < TOL);
 }
 
 QTEST_MAIN(TestGestureAlgos)
