d52e3b5d3df732cdcd0e6bea302f3d421ec95ff4
Bogdan Cristea <cristeab@gmail.com>
2013-08-19 13:06:09 +0200

    corrected splash screen

--- main.cpp
+++ main.cpp
@@ -16,13 +16,20 @@ int main(int argc, char *argv[])
 		painter.setPen(Qt::darkBlue);
 		QFont font("Arial", 30);
 		painter.setFont(font);
-		int posX = 40;
-		int posY = 250;
+		QSize size = splashImg.size();
+		int posX = size.width()/6;
+		int posY = size.height()/2;
 		painter.drawText(posX, posY, "Virtual Touch Screen");
 		painter.setPen(Qt::blue);
+		painter.setFont(QFont("Arial", 15));
+		posY -= 30;
+		painter.drawText(posX, posY+100, "Available commands:");
+		painter.drawText(posX, posY+130, " - F1: help");
+		painter.drawText(posX, posY+160, " - F2: shows the configuration dialog");
+		painter.drawText(posX, posY+190, " - ctrl+q: quits the application");
 		painter.setFont(QFont("Arial", 12, -1, true));
-		posY = 300;
-		posX = 150;
+		posX = size.width()/3;
+		posY = 300;		
 		painter.drawText(posX, posY+150, "Copyright Bogdan Cristea");
 		painter.drawText(posX-10, posY+170, "e-mail: cristeab@gmail.com");
 	}
